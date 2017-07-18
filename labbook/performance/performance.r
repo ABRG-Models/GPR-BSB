d505d885d653cc9e1932b00f8377a6f620c0cd01
Seb James <seb.james@sheffield.ac.uk>
2017-07-18 13:26:27 +0100

    Adds performance results for impulse based 1 channel model

--- labbook/performance/performance.r
+++ labbook/performance/performance.r
@@ -2,7 +2,7 @@
 
 source('../bootstrap_functions.r')
 
-print ("***********  Single channel model  *************")
+print ("***********  Single channel event based model  *************")
 
 # Data
 bgbsb1_nolog_exe_tmp<- read.csv (file="bgbsb1_nolog_exe.csv", header=FALSE, sep=",");
@@ -34,7 +34,7 @@ print (sprintf ("1 channel nolog total vs. nolog exe. difference is %f, stderr: 
 
 
 
-print ("***********  Three channel model  *************")
+print ("***********  Three channel event based model  *************")
 
 # Data (three channels)
 bgbsb3_nolog_exe_tmp<- read.csv (file="bgbsb3_nolog_exe.csv", header=FALSE, sep=",");
@@ -63,3 +63,33 @@ print (sprintf ("3 channel log vs. nolog. Exe difference is %f, stderr: %f", log
 # Difference between nolog total and nolog exe gives pre-processing
 prediff <- b.diffste (bgbsb3_nolog_total, bgbsb3_nolog_exe, 256)
 print (sprintf ("3 channel nolog total vs. nolog exe. difference is %f, stderr: %f", prediff$meandiff, prediff$stderr))
+
+print ("***********  Single channel impulse based model  *************")
+
+# Data
+bgbsb1_impt_nolog_exe_tmp<- read.csv (file="bgbsb1_impt_nolog_exe.csv", header=FALSE, sep=",");
+bgbsb1_impt_nolog_exe <- bgbsb1_impt_nolog_exe_tmp$V1;
+
+bgbsb1_impt_withlog_exe_tmp<- read.csv (file="bgbsb1_impt_withlog_exe.csv", header=FALSE, sep=",");
+bgbsb1_impt_withlog_exe <- bgbsb1_impt_withlog_exe_tmp$V1;
+
+bgbsb1_impt_nolog_total_tmp<- read.csv (file="bgbsb1_impt_nolog_total.csv", header=FALSE, sep=",");
+bgbsb1_impt_nolog_total <- bgbsb1_impt_nolog_total_tmp$V1;
+
+bgbsb1_impt_withlog_total_tmp<- read.csv (file="bgbsb1_impt_withlog_total.csv", header=FALSE, sep=",");
+bgbsb1_impt_withlog_total <- bgbsb1_impt_withlog_total_tmp$V1;
+
+# Analysis
+bgbsb1_impt_nolog_exe_mean <- b.mean(bgbsb1_impt_nolog_exe, 256)
+print (sprintf ("1 channel impulse no logging: mean exec time: %f s, stderr %f", mean(bgbsb1_impt_nolog_exe), bgbsb1_impt_nolog_exe_mean$std.err))
+
+bgbsb1_impt_withlog_exe_mean <- b.mean(bgbsb1_impt_withlog_exe, 256)
+print (sprintf ("1 channel impulse with logging: mean exec time: %f s, stderr %f", mean(bgbsb1_impt_withlog_exe), bgbsb1_impt_withlog_exe_mean$std.err))
+
+# Difference between withlogging and nologging gives time for logging
+logdiff <- b.diffste (bgbsb1_impt_withlog_exe, bgbsb1_impt_nolog_exe, 256)
+print (sprintf ("1 channel impulse log vs. nolog. Exe difference is %f, stderr: %f", logdiff$meandiff, logdiff$stderr))
+
+# Difference between nolog total and nolog exe gives pre-processing
+prediff <- b.diffste (bgbsb1_impt_nolog_total, bgbsb1_impt_nolog_exe, 256)
+print (sprintf ("1 channel impulse nolog total vs. nolog exe. difference is %f, stderr: %f", prediff$meandiff, prediff$stderr))
---
+++ labbook/performance/run_bgbsb1_impt_nolog.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+MODEL=bgbsb1_impt
+EXPT=5
+LOGFILE=${MODEL}_nolog
+echo ${LOGFILE}
+for i in {1..10}; do
+    echo "Run $i" >> ${LOGFILE}.txt
+    /usr/bin/time -f "Totalseconds: %e" -o timeout.txt ./convert_script_s2b -m ~/models/GPR-BSB/${MODEL} -e${EXPT} temp/${LOGFILE} | tail -n 2 >> ${LOGFILE}.txt
+
+    cat timeout.txt >> ${LOGFILE}.txt
+done
